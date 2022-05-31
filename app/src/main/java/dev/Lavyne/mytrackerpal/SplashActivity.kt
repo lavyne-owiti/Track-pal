8b90fd68072242256b56b1003c9bc3d25db029ec
lavyne-owiti <owitilavyne@gmail.com>
2022-05-31 07:46:24 +0300

    first

--- app/src/main/java/dev/Lavyne/mytrackerpal/SplashActivity.kt
+++ app/src/main/java/dev/Lavyne/mytrackerpal/SplashActivity.kt
@@ -1,11 +1,14 @@
 package dev.Lavyne.mytrackerpal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val intent =Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
